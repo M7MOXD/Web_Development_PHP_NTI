9e9b4e0d55393a8988f6c3a8f84de2f6624ede10
M7MOXD <mahmood.x.mostafa@gmail.com>
2022-03-07 18:59:33 +0200

    Update index.php

--- Task_ONE/index.php
+++ Task_ONE/index.php
@@ -7,7 +7,7 @@
         $remain = $units - ($f50 + $x100);
         $cost = ($f50 * 3.5) + ($x100 * 4) + ($remain * 6.5);
     } else {
-        $f50 = 50;
+        $f50 = $units <= 50 ? $units : 50;
         $remain = $units - $f50;
         if ($remain > 0) {
             $cost = ($f50 * 3.5) + ($remain * 4);
