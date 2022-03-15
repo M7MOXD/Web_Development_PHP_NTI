d0114e2fe516f2a3d379a46b57e1515c845ed137
M7MOXD <mahmood.x.mostafa@gmail.com>
2022-03-15 18:34:02 +0200

    Update remove.php

--- Task_Seven/remove.php
+++ Task_Seven/remove.php
@@ -1,11 +1,16 @@
 <?php
     require_once "dbConnection.php";
     $id = $_GET["id"];
+    $sql = "SELECT * FROM articles WHERE id=$id";
+    $op = mysqli_query($connect, $sql);
+    $data = mysqli_fetch_assoc($op);
     $sql = "DELETE FROM articles WHERE id=$id";
     $op = mysqli_query($connect, $sql);
     mysqli_close($connect);
     if ($op) {
-        echo "Row Deleted";
+        $message = "Row Deleted";
+        $_SESSION["message"] = $message;
+        unlink($data["image"]);
         header("location: articles.php");
     } else {
         echo "Error: Try Again".mysqli_error($connect);
